7c6d87e326038ae1790b90943e418eff4f709e2d
Filipe Patricio <facpatricio@gmail.com>
2021-03-08 15:09:07 +0000

    Add Pods

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'DogSword' do
+  use_frameworks!
+  pod 'Alamofire', '~> 5.0'
+end
---
+++ Pods/Target Support Files/Pods-DogSword/Pods-DogSword-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DogSword : NSObject
+@end
+@implementation PodsDummy_Pods_DogSword
+@end
